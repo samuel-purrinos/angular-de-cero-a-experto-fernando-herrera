a85476568fea726701f0b7aea5f0ddfe1da21758
Samuel Purriños <samuel.purrinos@outlook.com>
2024-04-20 21:10:35 +0200

    GifsApp - Sección 7

---
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  public gifList : Gif[] =[];
+
+  private apiKey : string = '18SJJv1S6B4YKKiy8CEUG5G0CRneTjQR';
+  private serviceUrl = 'https://api.giphy.com/v1/gifs'
+
+  private _tagsHistory : string[] = [];
+
+  constructor(private http : HttpClient) { }
+
+  get tagsHistory(){
+    return [...this._tagsHistory];
+  }
+
+  searchTag(tag : string){
+    if(tag.length == 0) return;
+    this.organizeHistory(tag);
+    const params = new HttpParams()
+    .set('api_key',this.apiKey)
+    .set('limit',10)
+    .set('q',tag);
+    this.http.get<SearchResponse>(`${this.serviceUrl}`+'/search',{params})
+    .subscribe(resp => {
+      this.gifList=resp.data;
+      console.log({gifs:this.gifList})
+    });
+  }
+
+  private organizeHistory(tag : string){
+    tag= tag.toLowerCase();
+    if(this.tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !=tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+}
