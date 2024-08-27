fe4c0776acd61b13870a7b3ae48f911747260ec1
Samuel Purriños <samuel.purrinos@outlook.com>
2024-08-27 15:18:01 +0200

    Sección 14 - HeroesApp con angular material

---
+++ 06-heroes-app/heroesApp/src/app/heroes/pages/list-page/list-page.component.ts
@@ -0,0 +1,23 @@
+import { HeroesService } from '../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'app-list-page',
+  templateUrl: './list-page.component.html',
+  styles: [
+  ]
+})
+export class ListPageComponent implements OnInit{
+
+  public heroes: Hero[] = [];
+
+  constructor(private  heroesService  : HeroesService){}
+  ngOnInit(): void {
+    this.heroesService.getHeroes()
+    .subscribe(heroes =>
+      this.heroes=heroes
+    );
+  }
+
+}
